76dfeb883a0593f4ec75213b4638e5691ef4abc3
Rajath Subramanyam <rajathsiyer@gmail.com>
2021-09-03 17:13:59 -0400

    Built basic gui, with buttons

--- src/application/SampleController.java
+++ src/application/SampleController.java
@@ -1,5 +1,154 @@
 package application;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class SampleController {
+	@FXML
+	private Button button0;
+	@FXML
+	private Button button1;
+	@FXML
+	private Button button2;
+	@FXML
+	private Button button3;
+	@FXML
+	private Button button4;
+	@FXML
+	private Button button5;
+	@FXML
+	private Button button6;
+	@FXML
+	private Button button7;
+	@FXML
+	private Button button8;
+	@FXML
+	private Button button9;
+	@FXML
+	private Button button10;
+	@FXML
+	private Button button11;
+	@FXML
+	private Button button12;
+	@FXML
+	private Button button13;
+	@FXML
+	private Button button14;
+	@FXML
+	private Button button15;
+	@FXML
+	private Button button16;
+	@FXML
+	private Button button17;
+	@FXML
+	private Button button18;
+	@FXML
+	private Button button19;
+	@FXML
+	private Button button20;
+	@FXML
+	private Button button21;
+	@FXML
+	private Button button22;
+	@FXML
+	private Button button23;
+	@FXML
+	private Button button24;
+	@FXML
+	private Button button25;
+	@FXML
+	private Button button26;
+	@FXML
+	private Button button27;
+	@FXML
+	private Button button28;
+	@FXML
+	private Button button29;
+	@FXML	
+	private Button button30;
+	@FXML
+	private Button button31;
+	@FXML
+	private Button button32;
+	@FXML
+	private Button button33;
+	@FXML
+	private Button button34;
+	@FXML
+	private Button button35;
+	@FXML
+	private Button button36;
+	@FXML
+	private Button button37;
+	@FXML
+	private Button button38;
+	@FXML
+	private Button button39;
+	@FXML	
+	private Button button40;
+	@FXML
+	private Button button41;
+	@FXML
+	private Button button42;
+	@FXML
+	private Button button43;
+	@FXML
+	private Button button44;
+	@FXML
+	private Button button45;
+	@FXML
+	private Button button46;
+	@FXML
+	private Button button47;
+	@FXML
+	private Button button48;
+	@FXML
+	private Button button49;
+	@FXML
+	private Button button50;
+	@FXML
+	private Button button51;
+	@FXML
+	private Button button52;
+	@FXML
+	private Button button53;
+	@FXML
+	private Button button54;
+	@FXML
+	private Button button55;
+	@FXML
+	private Button button56;
+	@FXML
+	private Button button57;
+	@FXML
+	private Button button58;
+	@FXML
+	private Button button59;
+	@FXML
+	private Button button60;
+	@FXML
+	private Button button61;
+	@FXML
+	private Button button62;
+	@FXML
+	private Button button63;
 	
+	@FXML
+	private void handleButtonAction(ActionEvent e)
+	{
+		if (e.getSource() == button1) {
+			System.out.println("yeeeee1");
+	        System.out.println("1");
+	    } else if (e.getSource() == button2) {
+	    	System.out.println("yeeeee2");
+	    } else if (e.getSource() == button3) {
+	    	System.out.println("yeeeee3");
+	    } else if (e.getSource() == button4) {
+	    	System.out.println("yeeeee4");
+	    } else if (e.getSource() == button5) {
+	    	System.out.println("yeeeee5");
+	    }
+
+	}
 }
