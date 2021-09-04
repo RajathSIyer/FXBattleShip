105cf79b06f297a1c22f7fdaa3b4efe2c704d1ad
rajan-code <65838883+rajan-code@users.noreply.github.com>
2021-09-03 20:00:37 -0400

    Implemented getting the row and column when a button is clicked

--- src/application/SampleController.java
+++ src/application/SampleController.java
@@ -135,20 +135,202 @@ public class SampleController {
 	private Button button63;
 	
 	@FXML
-	private void handleButtonAction(ActionEvent e)
-	{
-		if (e.getSource() == button1) {
-			System.out.println("yeeeee1");
-	        System.out.println("1");
-	    } else if (e.getSource() == button2) {
-	    	System.out.println("yeeeee2");
-	    } else if (e.getSource() == button3) {
-	    	System.out.println("yeeeee3");
-	    } else if (e.getSource() == button4) {
-	    	System.out.println("yeeeee4");
-	    } else if (e.getSource() == button5) {
-	    	System.out.println("yeeeee5");
-	    }
+	private void handleButtonAction(ActionEvent e) {
+		int row = 0, col = 0;
+		if (e.getSource() == button0) {
+			row = 0;
+			col = 0;
+		} else if (e.getSource() == button1) {
+		    row = 0;
+		    col = 1;
+		} else if (e.getSource() == button2) {
+		    row = 0;
+		    col = 2;
+		} else if (e.getSource() == button3) {
+		    row = 0;
+		    col = 3;
+		} else if (e.getSource() == button4) {
+		    row = 0;
+		    col = 4;
+		} else if (e.getSource() == button5) {
+		    row = 0;
+		    col = 5;
+		} else if (e.getSource() == button6) {
+		    row = 0;
+		    col = 6;
+		} else if (e.getSource() == button7) {
+		    row = 0;
+		    col = 7;
+		} else if (e.getSource() == button8) {
+		    row = 1;
+		    col = 0;
+		} else if (e.getSource() == button9) {
+		    row = 1;
+		    col = 1;
+		} else if (e.getSource() == button10) {
+		    row = 1;
+		    col = 2;
+		} else if (e.getSource() == button11) {
+		    row = 1;
+		    col = 3;
+		} else if (e.getSource() == button12) {
+		    row = 1;
+		    col = 4;
+		} else if (e.getSource() == button13) {
+		    row = 1;
+		    col = 5;
+		} else if (e.getSource() == button14) {
+		    row = 1;
+		    col = 6;
+		} else if (e.getSource() == button15) {
+		    row = 1;
+		    col = 7;
+		} else if (e.getSource() == button16) {
+		    row = 2;
+		    col = 0;
+		} else if (e.getSource() == button17) {
+		    row = 2;
+		    col = 1;
+		} else if (e.getSource() == button18) {
+		    row = 2;
+		    col = 2;
+		} else if (e.getSource() == button19) {
+		    row = 2;
+		    col = 3;
+		} else if (e.getSource() == button20) {
+		    row = 2;
+		    col = 4;
+		} else if (e.getSource() == button21) {
+		    row = 2;
+		    col = 5;
+		} else if (e.getSource() == button22) {
+		    row = 2;
+		    col = 6;
+		} else if (e.getSource() == button23) {
+		    row = 2;
+		    col = 7;
+		} else if (e.getSource() == button24) {
+		    row = 3;
+		    col = 0;
+		} else if (e.getSource() == button25) {
+		    row = 3;
+		    col = 1;
+		} else if (e.getSource() == button26) {
+		    row = 3;
+		    col = 2;
+		} else if (e.getSource() == button27) {
+		    row = 3;
+		    col = 3;
+		} else if (e.getSource() == button28) {
+		    row = 3;
+		    col = 4;
+		} else if (e.getSource() == button29) {
+		    row = 3;
+		    col = 5;
+		} else if (e.getSource() == button30) {
+		    row = 3;
+		    col = 6;
+		} else if (e.getSource() == button31) {
+		    row = 3;
+		    col = 7;
+		} else if (e.getSource() == button32) {
+		    row = 4;
+		    col = 0;
+		} else if (e.getSource() == button33) {
+		    row = 4;
+		    col = 1;
+		} else if (e.getSource() == button34) {
+		    row = 4;
+		    col = 2;
+		} else if (e.getSource() == button35) {
+		    row = 4;
+		    col = 3;
+		} else if (e.getSource() == button36) {
+		    row = 4;
+		    col = 4;
+		} else if (e.getSource() == button37) {
+		    row = 4;
+		    col = 5;
+		} else if (e.getSource() == button38) {
+		    row = 4;
+		    col = 6;
+		} else if (e.getSource() == button39) {
+		    row = 4;
+		    col = 7;
+		} else if (e.getSource() == button40) {
+		    row = 5;
+		    col = 0;
+		} else if (e.getSource() == button41) {
+		    row = 5;
+		    col = 1;
+		} else if (e.getSource() == button42) {
+		    row = 5;
+		    col = 2;
+		} else if (e.getSource() == button43) {
+		    row = 5;
+		    col = 3;
+		} else if (e.getSource() == button44) {
+		    row = 5;
+		    col = 4;
+		} else if (e.getSource() == button45) {
+		    row = 5;
+		    col = 5;
+		} else if (e.getSource() == button46) {
+		    row = 5;
+		    col = 6;
+		} else if (e.getSource() == button47) {
+		    row = 5;
+		    col = 7;
+		} else if (e.getSource() == button48) {
+		    row = 6;
+		    col = 0;
+		} else if (e.getSource() == button49) {
+		    row = 6;
+		    col = 1;
+		} else if (e.getSource() == button50) {
+		    row = 6;
+		    col = 2;
+		} else if (e.getSource() == button51) {
+		    row = 6;
+		    col = 3;
+		} else if (e.getSource() == button52) {
+		    row = 6;
+		    col = 4;
+		} else if (e.getSource() == button53) {
+		    row = 6;
+		    col = 5;
+		} else if (e.getSource() == button54) {
+		    row = 6;
+		    col = 6;
+		} else if (e.getSource() == button55) {
+		    row = 6;
+		    col = 7;
+		} else if (e.getSource() == button56) {
+		    row = 7;
+		    col = 0;
+		} else if (e.getSource() == button57) {
+		    row = 7;
+		    col = 1;
+		} else if (e.getSource() == button58) {
+		    row = 7;
+		    col = 2;
+		} else if (e.getSource() == button59) {
+		    row = 7;
+		    col = 3;
+		} else if (e.getSource() == button60) {
+		    row = 7;
+		    col = 4;
+		} else if (e.getSource() == button61) {
+		    row = 7;
+		    col = 5;
+		} else if (e.getSource() == button62) {
+		    row = 7;
+		    col = 6;
+		} else if (e.getSource() == button63) {
+		    row = 7;
+		    col = 7;
+		}
+		System.out.println("Row: " + row + " Column: " + col);
 
 	}
 }
